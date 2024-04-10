9c2dcd6b17fb3dc5125789c7838686c370040356
yash <yashfuturisticinc@gmail.com>
2024-04-10 10:36:09 +0530

    Add the Edit Button

--- app/issues/[id]/page.tsx
+++ app/issues/[id]/page.tsx
@@ -5,8 +5,12 @@ import React, { useState } from "react";
 import {
   Badge,
   Blockquote,
+  Box,
+  Button,
   Callout,
   Card,
+  Flex,
+  Grid,
   Heading,
   Text,
 } from "@radix-ui/themes";
@@ -16,6 +20,8 @@ import IssueStatusBadge from "@/app/components/IssueStatusBadge";
 import { notFound } from "next/navigation";
 import ReactMarkdown from "react-markdown";
 import delay from "delay";
+import { Pencil2Icon } from "@radix-ui/react-icons";
+import Link from "next/link";
 
 type Props = {
   params: { id: string };
@@ -41,7 +47,8 @@ const page = async ({ params: { id } }: Props) => {
 
   await delay(2000);
   return (
-    <div className="max-w-xl space-y-3">
+    <Grid columns={{ initial: "1", md: "2" }} gap={"3"}>
+      {/* maxWidth={"50vw"} */}
       {/* {error && (
         <Callout.Root variant="surface" color="plum">
           <InfoCircledIcon />
@@ -50,21 +57,29 @@ const page = async ({ params: { id } }: Props) => {
           </Callout.Text>
         </Callout.Root>
       )} */}
-      <Heading>
-        Issue {issue?.id} | {issue?.title}
-      </Heading>
-      <div className="flex flex-row space-x-3">
-        <IssueStatusBadge status={issue!.status} />
-        <Text size={"2"}>{issue?.createdAt.toLocaleString()}</Text>
-      </div>
-      {/* <Blockquote size="2">{issue?.description}</Blockquote> */}
-      {/* We want to ideally use react-markdown component in order to make the user specified 'markdown' in the issue's description field visible here */}
-      <Card className="prose" variant="classic">
-        {/* <Blockquote size={"2"}> */}
-        <ReactMarkdown>{issue?.description}</ReactMarkdown>
-        {/* </Blockquote> */}
-      </Card>
-    </div>
+      <Box>
+        <Heading>
+          Issue {issue?.id} | {issue?.title}
+        </Heading>
+        <Flex className="space-x-3" my="3">
+          <IssueStatusBadge status={issue!.status} />
+          <Text size={"2"}>{issue?.createdAt.toLocaleString()}</Text>
+        </Flex>
+        {/* <Blockquote size="2">{issue?.description}</Blockquote> */}
+        {/* We want to ideally use react-markdown component in order to make the user specified 'markdown' in the issue's description field visible here */}
+        <Card className="prose" variant="classic">
+          {/* <Blockquote size={"2"}> */}
+          <ReactMarkdown>{issue?.description}</ReactMarkdown>
+          {/* </Blockquote> */}
+        </Card>
+      </Box>
+      <Box>
+        <Button>
+          <Pencil2Icon />
+          <Link href={`/issues/${issue.id}/edit`}></Link>Edit Issue
+        </Button>
+      </Box>
+    </Grid>
   );
 };
 
