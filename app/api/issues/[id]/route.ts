4bbeae65019a2f0e38f137f610fd8d03d127b88e
yash <yashfuturisticinc@gmail.com>
2024-04-11 19:19:23 +0530

    Build an API for deleting issues

--- app/api/issues/[id]/route.ts
+++ app/api/issues/[id]/route.ts
@@ -2,7 +2,7 @@ import prisma from "@/prisma/client";
 import { Issue } from "@prisma/client";
 import { NextRequest, NextResponse } from "next/server";
 import { createIssueSchema } from "@/app/validationSchemas";
-import { z } from "zod";
+import { any, z } from "zod";
 export async function PATCH(
   request: NextRequest,
   { params: { id } }: { params: { id: string } }
@@ -61,5 +61,53 @@ export async function PATCH(
       updatedIssue: updatedIssue,
     },
     { status: 202, statusText: "Object updated susccessfully" }
+  );
+}
+
+export async function DELETE(
+  request: NextRequest,
+  { params: { id } }: { params: { id: string } }
+) {
+  let issue = {} as Issue | null;
+  let body = undefined;
+  try {
+    issue = await prisma.issue.findUnique({ where: { id: parseInt(id) } });
+  } catch (error: any) {
+    return NextResponse.json(
+      { error: `No such Issue Object with the Id: ${id} found!` },
+      { status: 404, statusText: "Resource not found" }
+    );
+  }
+  if (typeof issue?.id == "undefined") {
+    return NextResponse.json(
+      { error: `No such Issue Object with the Id: ${id} found!` },
+      { status: 404, statusText: "Resource not found" }
+    );
+  }
+  if (request.body !== null) {
+    try {
+      body = await request.json();
+    } catch (error: any) {
+      return NextResponse.json(
+        {
+          error: `Please don't provide a body in the DEL request for issue: ${id}`,
+        },
+        { status: 400, statusText: "Invalid Request Object" }
+      );
+    }
+    if (Object.getOwnPropertyNames(issue).length !== 0) {
+      return NextResponse.json(
+        { error: "The DEL request body should not contain any data" },
+        { status: 400, statusText: "Invalid Request" }
+      );
+    }
+  }
+
+  const deletedIssue = await prisma.issue.delete({
+    where: { id: issue?.id },
+  });
+  return NextResponse.json(
+    { message: "Issue deleted successfully", deletedIssue: deletedIssue },
+    { status: 200, statusText: "Deletion Successful" }
   );
 }
