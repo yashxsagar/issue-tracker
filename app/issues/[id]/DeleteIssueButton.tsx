0a6a26e46e33433d15893913b5a3c7f82c73b63d
yash <yashfuturisticinc@gmail.com>
2024-04-11 18:19:24 +0530

    Show a confirmation dialog for delete action

--- app/issues/[id]/DeleteIssueButton.tsx
+++ app/issues/[id]/DeleteIssueButton.tsx
@@ -1,5 +1,6 @@
-import { TrashIcon } from "@radix-ui/react-icons";
-import { Button } from "@radix-ui/themes";
+"use client";
+import { ExitIcon, TrashIcon } from "@radix-ui/react-icons";
+import { AlertDialog, Button, Flex } from "@radix-ui/themes";
 import React from "react";
 
 interface Props {
@@ -8,10 +9,33 @@ interface Props {
 
 const DeleteIssueButton = ({ issueId }: Props) => {
   return (
-    <Button color="red">
-      <TrashIcon />
-      Delete Issue
-    </Button>
+    // Now let's wrap the DeleteIssueButton inside of an AlerDialog box fromn 'radix-ui'
+
+    <AlertDialog.Root>
+      <AlertDialog.Trigger>
+        <Button color="red">
+          <TrashIcon />
+          Delete Issue
+        </Button>
+      </AlertDialog.Trigger>
+      <AlertDialog.Content>
+        <AlertDialog.Title>Confirm Deletion</AlertDialog.Title>
+        <AlertDialog.Description>
+          Are you sure want to Delete Issue: {issueId}?
+        </AlertDialog.Description>
+        <Flex gap={"4"} mt="3">
+          <AlertDialog.Cancel>
+            <Button color="gray">Cancel Delete</Button>
+          </AlertDialog.Cancel>
+          <AlertDialog.Action>
+            <Button color="red">
+              <ExitIcon />
+              Delete Issue
+            </Button>
+          </AlertDialog.Action>
+        </Flex>
+      </AlertDialog.Content>
+    </AlertDialog.Root>
   );
 };
 
