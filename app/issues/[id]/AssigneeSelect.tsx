af7420eaefa9bfef5c659dc1d85d1bb2994a75a7
yash <yashfuturisticinc@gmail.com>
2024-04-16 13:07:01 +0530

    Fetch data with React Query!

--- app/issues/[id]/AssigneeSelect.tsx
+++ app/issues/[id]/AssigneeSelect.tsx
@@ -5,37 +5,60 @@ import axios from "axios";
 import React, { useEffect, useState } from "react";
 import _ from "lodash";
 import { DotFilledIcon } from "@radix-ui/react-icons";
+import { useQuery } from "@tanstack/react-query";
+import Skeleton from "@/app/components/Skeleton";
 
 const AssigneeSelect = () => {
-  const [users, setUsers] = useState<User[]>([]);
-  const [errors, setErrors] = useState([]);
-  //We cannot make the callback function of the useEffect hook async because react does not permit taht
-  //So we need to modularize the implementation of the useEffect hook inside of another sub-function fecthUsers
-  useEffect(() => {
-    const fecthUsers = async () => {
-      try {
-        const { data } = await axios.get<User[]>("/api/users");
-        setUsers(data);
-      } catch (error: any) {
-        console.log(error.response.data.errors);
-        setErrors(error);
-        <AlertDialog.Root>
-          <AlertDialog.Trigger>{errors.length > 0}</AlertDialog.Trigger>
-          <AlertDialog.Content>
-            <AlertDialog.Title>
-              Error fetching Users. Please check your request Header, Body or
-              Cookies
-            </AlertDialog.Title>
-            <AlertDialog.Description>
-              {error.response.data.errors}
-            </AlertDialog.Description>
-          </AlertDialog.Content>
-          <AlertDialog.Cancel>OK</AlertDialog.Cancel>
-        </AlertDialog.Root>;
-      }
-    };
-    fecthUsers();
-  }, []);
+  const {
+    data: users,
+    isLoading,
+    error,
+  } = useQuery<User[], Error>({
+    queryKey: ["users"],
+    queryFn: () => axios.get("/api/users").then((res) => res.data),
+    staleTime: 60 * 1000, //60s
+    retry: 3, //reactQuery will try automartically upto 3 times to fetch the data upon encountering any error(s)
+  });
+  //   With ReactQuery we no longer need to use state and effect hooks as it is a managed library that does all this under the hood
+  //   const [users, setUsers] = useState<User[]>([]);
+  //   const [errors, setErrors] = useState([]);
+  //   //We cannot make the callback function of the useEffect hook async because react does not permit taht
+  //   //So we need to modularize the implementation of the useEffect hook inside of another sub-function fecthUsers
+  //   useEffect(() => {
+  //     const fecthUsers = async () => {
+  //       try {
+  //         const { data } = await axios.get<User[]>("/api/users");
+  //         setUsers(data);
+  //       } catch (error: any) {
+  //         console.log(error.response.data.errors);
+  //         setErrors(error);
+  //         <AlertDialog.Root>
+  //           <AlertDialog.Trigger>{errors.length > 0}</AlertDialog.Trigger>
+  //           <AlertDialog.Content>
+  //             <AlertDialog.Title>
+  //               Error fetching Users. Please check your request Header, Body or
+  //               Cookies
+  //             </AlertDialog.Title>
+  //             <AlertDialog.Description>
+  //               {error.response.data.errors}
+  //             </AlertDialog.Description>
+  //           </AlertDialog.Content>
+  //           <AlertDialog.Cancel>OK</AlertDialog.Cancel>
+  //         </AlertDialog.Root>;
+  //       }
+  //     };
+  //     fecthUsers();
+  //   }, []);
+  if (error) {
+    return (
+      <Text as="label" align="center">
+        Error fetching Users
+      </Text>
+    );
+  }
+  if (isLoading) {
+    return <Skeleton />;
+  }
   return (
     <Select.Root>
       <Select.Trigger placeholder="Assign Member" />
@@ -43,9 +66,9 @@ const AssigneeSelect = () => {
         <Select.Group>
           <Select.Label>Suggestions</Select.Label>
           {users &&
-            users.map((user, index) => (
+            users?.map((user, index) => (
               <Select.Item key={user.id} value={user.id}>
-                {users.filter((u) => u.name == user.name).length > 1 ? (
+                {users?.filter((u) => u.name == user.name).length > 1 ? (
                   <HoverCard.Root>
                     <HoverCard.Trigger>
                       <Flex>
