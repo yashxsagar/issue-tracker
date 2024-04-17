fe8317a8d284d0835a8da48ee0889a2244d29c0f
yash <yashfuturisticinc@gmail.com>
2024-04-17 17:56:33 +0530

    Show toast notification on User Assignment

--- app/issues/[id]/AssigneeSelect.tsx
+++ app/issues/[id]/AssigneeSelect.tsx
@@ -10,6 +10,7 @@ import Skeleton from "@/app/components/Skeleton";
 import { LoadingSpinner } from "@/app/components";
 import delay from "delay";
 import { HiUserRemove } from "react-icons/hi";
+import toast, { Toaster } from "react-hot-toast";
 
 const AssigneeSelect = ({ issue }: { issue: Issue }) => {
   const [isAssigning, setIsAssigning] = useState(false);
@@ -71,96 +72,108 @@ const AssigneeSelect = ({ issue }: { issue: Issue }) => {
     return <Skeleton />;
   }
   return (
-    <Select.Root
-      onValueChange={(userId) => {
-        setSelectedUserId(userId);
-        setIsAssigning(true);
-        // delay(1000);
-        try {
-          axios.patch(`/api/issues/${issue.id}`, {
-            assignedToUserId: userId === "unassigned" ? null : userId,
-          });
-        } catch (error: any) {
-          console.log(error.response.data.errors);
-        } finally {
-          setIsAssigning(false);
+    <>
+      <Select.Root
+        onValueChange={async (userId) => {
+          let updatedIssue = {} as Issue;
+          setSelectedUserId(userId);
+          setIsAssigning(true);
+          // delay(1000);
+          try {
+            updatedIssue = await axios.patch(`/api/issues/${issue.id}`, {
+              assignedToUserId: userId === "unassigned" ? null : userId,
+            });
+          } catch (error: any) {
+            toast.error("New user could not be assigned!");
+            console.log(error.response.data.errors);
+          } finally {
+            setIsAssigning(false);
+            if (Object.keys(updatedIssue).length > 3) {
+              if (userId === "unassigned") {
+                toast("User unassigned");
+              } else {
+                toast.success(`New user assigned to Issue ${issue.id}`);
+              }
+            }
+          }
+        }}
+        defaultValue={
+          (issue.assignedToUserId &&
+            users?.find((user) => {
+              return user.id === issue.assignedToUserId;
+            })?.id) ||
+          // undefined --> to display 'Suggestions' as the default value
+          "unassigned"
         }
-      }}
-      defaultValue={
-        (issue.assignedToUserId &&
-          users?.find((user) => {
-            return user.id === issue.assignedToUserId;
-          })?.id) ||
-        // undefined --> to display 'Suggestions' as the default value
-        "unassigned"
-      }
-    >
-      <Select.Trigger placeholder="Assign Member" />
-      <Select.Content>
-        <Select.Group>
-          <Select.Label>Suggestions</Select.Label>
-          <Select.Item value="unassigned">
-            <Flex align={"center"} width={"11rem"} justify={"between"}>
-              <Text>Unassigned</Text> <HiUserRemove color={"plum"} />
-            </Flex>
-          </Select.Item>
-          {users &&
-            users?.map((user, index) => (
-              <Select.Item
-                key={user.id}
-                value={user.id}
-                onSelect={() => {
-                  console.log("Assigned user nullified");
-                  if (selectedUserId === user.id) {
-                    try {
-                      axios.patch(`/api/issues/${issue.id}`, {
-                        assignedToUserId: null,
-                      });
-                    } catch (error: any) {
-                      console.log(error.response.data.errors);
-                    } finally {
-                      console.log("Assigned user nullified");
+      >
+        <Select.Trigger placeholder="Assign Member" />
+        <Select.Content>
+          <Select.Group>
+            <Select.Label>Suggestions</Select.Label>
+            <Select.Item value="unassigned">
+              <Flex align={"center"} width={"11rem"} justify={"between"}>
+                <Text>Unassigned</Text> <HiUserRemove color={"plum"} />
+              </Flex>
+            </Select.Item>
+            {users &&
+              users?.map((user, index) => (
+                <Select.Item
+                  key={user.id}
+                  value={user.id}
+                  onSelect={() => {
+                    console.log("Assigned user nullified");
+                    if (selectedUserId === user.id) {
+                      try {
+                        axios.patch(`/api/issues/${issue.id}`, {
+                          assignedToUserId: null,
+                        });
+                      } catch (error: any) {
+                        console.log(error.response.data.errors);
+                      } finally {
+                        console.log("Assigned user nullified");
+                      }
                     }
-                  }
-                }}
-              >
-                {users?.filter((u) => u.name == user.name).length > 1 ? (
-                  <HoverCard.Root>
-                    <HoverCard.Trigger>
-                      <Flex justify="between" width="11rem">
-                        <Text>
-                          {user.name} {isAssigning && <LoadingSpinner />}
-                        </Text>
-                        <DotFilledIcon color={"plum"} height={"22"} />
-                      </Flex>
-                    </HoverCard.Trigger>
-                    <HoverCard.Content
-                      //   maxWidth="300px"
-                      size={"1"}
-                      side="right"
-                      //   height={"50px"}
-                      align="center"
-                      style={{
-                        backgroundColor: "white",
-                        color: "plum",
-                        borderStyle: "solid",
-                        borderWidth: "1px",
-                        borderColor: "plum",
-                      }}
-                    >
-                      <Text>{user.email}</Text>
-                    </HoverCard.Content>
-                  </HoverCard.Root>
-                ) : (
-                  <Text>
-                    {user.name} {isAssigning && <LoadingSpinner />}
-                  </Text>
-                )}
-              </Select.Item>
-            ))}
-        </Select.Group>
-      </Select.Content>
-    </Select.Root>
+                  }}
+                >
+                  {users?.filter((u) => u.name == user.name).length > 1 ? (
+                    <HoverCard.Root>
+                      <HoverCard.Trigger>
+                        <Flex justify="between" width="11rem">
+                          <Text>
+                            {user.name} {isAssigning && <LoadingSpinner />}
+                          </Text>
+                          <DotFilledIcon color={"plum"} height={"22"} />
+                        </Flex>
+                      </HoverCard.Trigger>
+                      <HoverCard.Content
+                        //   maxWidth="300px"
+                        size={"1"}
+                        side="right"
+                        //   height={"50px"}
+                        align="center"
+                        style={{
+                          backgroundColor: "white",
+                          color: "plum",
+                          borderStyle: "solid",
+                          borderWidth: "1px",
+                          borderColor: "plum",
+                        }}
+                      >
+                        <Text>{user.email}</Text>
+                      </HoverCard.Content>
+                    </HoverCard.Root>
+                  ) : (
+                    <Text>
+                      {user.name} {isAssigning && <LoadingSpinner />}
+                    </Text>
+                  )}
+                </Select.Item>
+              ))}
+          </Select.Group>
+        </Select.Content>
+      </Select.Root>
+      <Toaster />
+    </>
   );
 };
 
