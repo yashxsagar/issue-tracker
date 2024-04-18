687794d84e8342ba1bf71eac70e70eed5a33b454
yash <yashfuturisticinc@gmail.com>
2024-04-18 17:27:44 +0530

    Implement Pagination

--- app/components/Pagination.tsx
+++ app/components/Pagination.tsx
@@ -1,3 +1,4 @@
+"use client";
 import {
   ChevronLeftIcon,
   ChevronRightIcon,
@@ -5,6 +6,7 @@ import {
   DoubleArrowRightIcon,
 } from "@radix-ui/react-icons";
 import { Button, Flex, Text } from "@radix-ui/themes";
+import { useRouter, useSearchParams } from "next/navigation";
 import React from "react";
 
 interface Props {
@@ -13,39 +15,57 @@ interface Props {
   currentPage: number;
 }
 const Pagination = ({ countItems, currentPage, pageSize }: Props) => {
+  const router = useRouter();
+  const searchParams = useSearchParams();
   const pageCount = Math.ceil(countItems / pageSize);
   if (pageCount == 1) {
     return null;
   }
+  const changePage = (page: Number) => {
+    const params = new URLSearchParams(searchParams);
+    params.set("page", page.toString());
+    router.push(`?${params.toString()}`); //So we are only updateing the query string and not the end-point. The useRouter hook will  retain the value of the URL/endPoint
+  };
   return (
     <Flex align="center" gap="2">
       <Text size="2">
         Page {currentPage} of {pageCount}
       </Text>
-      <Button color="gray" size="1" variant="soft" disabled={currentPage === 1}>
+      <Button
+        color="plum"
+        size="1"
+        variant="soft"
+        disabled={currentPage === 1}
+        onClick={() => changePage(1)}
+      >
         <DoubleArrowLeftIcon />
       </Button>
       <Button
-        color="gray"
+        color="plum"
         size={"1"}
         variant="soft"
         disabled={currentPage === 1}
+        onClick={() => changePage(currentPage - 1)}
       >
         <ChevronLeftIcon />
       </Button>
       <Button
-        color="gray"
+        color="plum"
         size={"1"}
         variant="soft"
         disabled={currentPage === pageCount}
+        onClick={() => {
+          changePage(currentPage + 1);
+        }}
       >
         <ChevronRightIcon />
       </Button>
       <Button
-        color="gray"
+        color="plum"
         size={"1"}
         variant="soft"
         disabled={currentPage === pageCount}
+        onClick={() => changePage(pageCount)}
       >
         <DoubleArrowRightIcon />
       </Button>
--- app/page.tsx
+++ app/page.tsx
@@ -2,12 +2,20 @@ import { Button } from "@radix-ui/themes";
 import Pagination from "./components/Pagination";
 import { DoubleArrowLeftIcon } from "@radix-ui/react-icons";
 
-export default function Home() {
+export default function Home({
+  searchParams: { page },
+}: {
+  searchParams: { page: string };
+}) {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
       <div>
         <h1 className="text-5xl">Hello World</h1>
-        <Pagination countItems={100} pageSize={7} currentPage={15} />
+        <Pagination
+          countItems={100}
+          currentPage={page ? parseInt(page) : 1}
+          pageSize={7}
+        />
       </div>
     </main>
   );
